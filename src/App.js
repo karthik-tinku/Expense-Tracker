f76d6b10a0a3fbb6056ff8e974d655e51f2716bf
karthik-tinku <92668960+karthik-tinku@users.noreply.github.com>
2024-09-12 16:27:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Header from "./components/Header";
+
+import './App.css';
+import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
+import AddTransaction from "./components/AddTransaction";
+
+import TransactionList from "./components/TransactionList";
+
+
+import { GlobalProvider } from "./context/GlobalState";
+
+function App() {
+  return (
+    <GlobalProvider>
+     <Header />
+     <div className="container">
+      <Balance />
+      <IncomeExpenses />
+      <TransactionList />
+      <AddTransaction />
+     </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
